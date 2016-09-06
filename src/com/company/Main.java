e3058a560710043afd470bc53e3976d86589f8dc
panteraT <pantera_tanka@mail.ru>
2016-09-06 17:31:01 +0300

    ftb66rtb

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println ("GOVNO");
+        System.out.println ("GOVNO22222222");
     }
 }
